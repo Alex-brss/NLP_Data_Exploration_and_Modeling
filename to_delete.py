76aab4f2455363ab9c6f88449033b649e7a6c349
Alexandre Cogordan <alexcogordan2001@hotmail.fr>
2024-01-20 15:17:48 +0100

    added all of the models, refining is needed

--- to_delete.py
+++ to_delete.py
@@ -20,8 +20,6 @@ import gensim.downloader as api
 import tensorflow_hub as hub
 
 from torch.utils.tensorboard import SummaryWriter
-from scipy.spatial.distance import euclidean
-from scipy.spatial.distance import cosine
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.util import ngrams
@@ -44,6 +42,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from tensorflow.keras.layers import Dense, LSTM
 from tensorflow.keras.models import Model, Sequential
+from scipy.spatial.distance import euclidean, cosine
 
 load_dotenv()
 np.random.seed(42)
@@ -106,14 +105,14 @@ def preprocessing(text):
 df['cleaned_text'] = df['text'].apply(preprocessing)
 df['tokens'] = tokenisation(df['cleaned_text'])
 
-# # Summarisation
+# Summarisation
 
-# max_length_coef = 1.5
-# min_length_coef = 2
+max_length_coef = 1.5
+min_length_coef = 2
 
-# summariser = pipeline("summarization", model="facebook/bart-large-cnn")
-# summarised_text = df['text'].apply(lambda x: summariser(x, max_length=round(len(x)/max_length_coef), min_length=round(len(x)/min_length_coef), do_sample=False))
-# df['summarised_text'] = summarised_text.apply(lambda x: x[0]['summary_text'])
+summariser = pipeline("summarization", model="facebook/bart-large-cnn")
+summarised_text = df['text'].apply(lambda x: summariser(x, max_length=round(len(x)/max_length_coef), min_length=round(len(x)/min_length_coef), do_sample=False))
+df['summarised_text'] = summarised_text.apply(lambda x: x[0]['summary_text'])
 
 # ---------- Highlighting frequent words ---------- #
 
@@ -157,6 +156,10 @@ lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
                                             passes=10,
                                             alpha='auto',
                                             per_word_topics=True)
+
+pyLDAvis.enable_notebook(local=True)
+vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word, mds='mmds', R=10)
+pyLDAvis.display(vis)
 
 def get_topic_distribution(lda_model, bow):
     return lda_model.get_document_topics(bow, minimum_probability=0)
@@ -206,12 +209,123 @@ df['top_topic_labels'] = df['top_topics'].apply(lambda x: label_topics(x, lda_mo
 df['topics'] = df['top_topic_labels'].apply(lambda x: topicise(x, label_dict))
 df.drop(columns=['topic_distribution', 'top_topics'], inplace=True)
 
-# ---------- Sentiment Analysis ---------- #
+# Phrases alogrithm
 
-# ---------- Word2Vec ---------- #
+min_count = 3
+threshold = 5
 
-# We train our own word2vec model
+phrases = Phrases(df['tokens'], min_count=min_count, threshold=threshold)
+phraser = Phraser(phrases)
 
-word2vec_model = Word2Vec(sentences=df['tokens'], vector_size=100, window=5, min_count=1, workers=4)
-word2vec_model.save("yelp_reviews.model")
+df['bigrams'] = [phraser[tokens] for tokens in df['tokens']]
+df['trigrams'] = [phraser[bigrams] for bigrams in df['bigrams']]
 
+vector_size = 100
+window = 5
+min_count = 1
+workers = 4
+
+# Training the model
+word2vec_model = Word2Vec(sentences=df['bigrams'], vector_size=vector_size, window=window, min_count=min_count, workers=workers)
+
+# Save the model
+word2vec_model.save("word2vec.model")
+
+def tsne_plot(model):
+    vocab = []
+    for i in range(0,len(model.wv)):
+        vocab.append(model.wv.index_to_key[i])
+
+    "Creates and TSNE model and plots it"
+    labels = []
+    tokens = []
+
+    for word in vocab:
+        tokens.append(model.wv[word])
+        labels.append(word)
+        #print(tokens)
+        #print(labels)
+    tokens = np.array(tokens)
+    tsne_model = TSNE(perplexity=200, n_components=2, init='pca', n_iter=2500, random_state=23)
+    new_values = tsne_model.fit_transform(tokens)
+
+
+    
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+    
+    
+    
+    plt.figure(figsize=(16, 16)) 
+    for i in range(len(x)):
+        plt.scatter(x[i],y[i])
+        plt.annotate(labels[i],
+                     xy=(x[i], y[i]),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
+    plt.show()
+    
+
+tsne_plot(word2vec_model)
+
+file_name = "word2vec.model"
+model = gensim.models.keyedvectors.KeyedVectors.load(file_name)
+
+max_size = len(model.wv.index_to_key)-1
+
+w2v = np.zeros((max_size,model.vector_size))
+
+if not os.path.exists('projections'):
+    os.makedirs('projections')
+    
+with open("projections/metadata.tsv", 'w+') as file_metadata:
+    
+    for i, word in enumerate(model.wv.index_to_key[:max_size]):
+        
+        #store the embeddings of the word
+        w2v[i] = model.wv[word]
+        
+        #write the word to a file 
+        file_metadata.write(word + '\n')
+
+tf.compat.v1.disable_eager_execution()
+sess = tf.compat.v1.InteractiveSession()
+
+with tf.device("/cpu:0"):
+    embedding = tf.Variable(w2v, trainable=False, name='embedding')
+
+sess.run(tf.compat.v1.global_variables_initializer())
+
+saver = tf.compat.v1.train.Saver()
+writer = tf.compat.v1.summary.FileWriter('projections', sess.graph)
+config = projector.ProjectorConfig()
+embed= config.embeddings.add()
+
+embed.tensor_name = 'embedding'
+embed.metadata_path = 'metadata.tsv'
+
+projector.visualize_embeddings(writer, config)
+saver.save(sess, 'projections/model.ckpt', global_step=max_size)
+
+def semantic_search(query_word, model, topn=10):
+    query_vector = model.wv[query_word]
+    all_words = model.wv.index_to_key
+
+    # Calculate cosine distance between query and all other words
+    distances = {word: cosine(query_vector, model.wv[word]) for word in all_words}
+    
+    # Sort words by distance (lower is more similar)
+    sorted_words = sorted(distances, key=distances.get)
+
+    # Return the topn closest words
+    return sorted_words[:topn]
+
+# Example usage
+search_results = semantic_search('wine', word2vec_model)
+
+search_results
